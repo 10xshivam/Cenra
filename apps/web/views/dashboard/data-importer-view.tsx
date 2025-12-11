ae25a2e0edd479f7ab4e76755b2534ded69567d8
Shivam <kumarshivam3788@gmail.com>
2025-12-12 00:19:22 +0530

    feat: implement Data Importer View with file upload functionality and add react-dropzone dependency

--- apps/web/views/dashboard/data-importer-view.tsx
+++ apps/web/views/dashboard/data-importer-view.tsx
@@ -1,7 +1,57 @@
+import Image from "next/image";
+import { CirclePlus } from "lucide-react";
+import { SquareInfo } from "@workspace/ui/components/icons";
+
 export const DataImporterView = () => {
   return (
     <div className="w-full h-full flex justify-center p-16">
-      Data Importer View Coming Soon!
+      <div className="bg-neutral-100 rounded-2xl w-full p-2 max-w-6xl h-fit border">
+        <div className="w-full border border-neutral-300 h-60 rounded-xl p-5 bg-neutral-50 flex flex-col justify-center gap-4">
+          <div className="flex gap-2 items-center">
+            <Image
+              width={60}
+              height={60}
+              alt="Knowledge Source"
+              src={"/file-icon.png"}
+            />
+            <div className="flex flex-col">
+              <h3 className="font-bold  tracking-tight mt-1">
+                Upload files
+              </h3>
+              <p className="text-xs text-neutral-500 tracking-tight font-medium">
+                Select documents from your device
+              </p>
+            </div>
+          </div>
+          <div className="w-full h-full rounded-lg flex gap-16 items-center ">
+            <div className="flex-1 border-2 h-full rounded-lg border-dashed bg-neutral-100/50 flex justify-center items-center flex-col hover:bg-emerald-100/5 transition-colors duration-300 cursor-pointer hover:border-emerald-600">
+              <CirclePlus
+                size={20}
+                strokeWidth={2.5}
+                className="text-neutral-500 mb-1.5"
+              />
+              <span className="font-medium text-sm text-neutral-500 tracking-tight mb-0.5">
+                Drag and drop document file here
+              </span>
+              <span className="text-xs text-neutral-400 tracking-tighter">
+                Accepted file types: .pdf, .csv, .txt
+              </span>
+            </div>
+            <div className="flex h-full border-l pl-5 flex-col gap-3.5 justify-center">
+              <span className="flex gap-1 text-sm items-center text-emerald-800 font-semibold tracking-tight">
+                <SquareInfo size="18" /> What file types are supported?
+              </span>
+              <span className="text-neutral-500 text-xs tracking-tight max-w-3xs">
+                You can upload any kind of text documents, such as PDF or CSV.
+              </span>
+              <span className="text-neutral-500 text-xs tracking-tight max-w-3xs">
+                Cenra will extract the text content from your documents and
+                train your AI with that text.
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- packages/ui/src/components/icons.tsx
+++ packages/ui/src/components/icons.tsx
@@ -930,3 +930,11 @@ export const WebCrawlIcon = (props: React.SVGProps<SVGSVGElement>) => (
     />
   </svg>
 );
+
+
+export const SquareInfo = ({size = '24px', ...props}: IconProps) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width={size} height={size} viewBox="0 0 24 24" {...props}><path d="m19,2H5c-1.654,0-3,1.346-3,3v14c0,1.654,1.346,3,3,3h14c1.654,0,3-1.346,3-3V5c0-1.654-1.346-3-3-3Zm-6,16h-2v-6h-2v-2h2.5c.827,0,1.5.673,1.5,1.5v6.5Zm-1-9.5c-.689,0-1.25-.561-1.25-1.25s.561-1.25,1.25-1.25,1.25.561,1.25,1.25-.561,1.25-1.25,1.25Z" strokeWidth="0" fill="currentColor"></path></svg>
+  );
+};
+
